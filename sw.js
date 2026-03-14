d6946d3570ab93c52188db37cfab500f64148cb6
Jan K <jan.kostulski@web.de>
2026-03-14 20:46:05 +0100

    fix: update version number in index.html and service worker cache

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Anki Scribe - Service Worker
 // Provides offline support via caching
 
-const CACHE_NAME = 'anki-scribe-v13';
+const CACHE_NAME = 'anki-scribe-v14';
 
 // App shell files to pre-cache on install
 const APP_SHELL = [
